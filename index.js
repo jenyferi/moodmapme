8ff8119e4fd8d3f95cb0d3b11d1fd135811fdc5e
jenyferi <jenyferi@gmail.com>
2012-08-13 16:14:34 -0700

    trying to add frontend 2

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ app.get('/', function(request, response) {
         response.send('Hello hello 3!');
         });
 
+app.engine('.html', require('jade'));
+
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
            console.log("Listening on " + port);
            });
