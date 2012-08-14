85175fff56f44317beb250bbbfa44b5ab51a4777
jenyferi <jenyferi@gmail.com>
2012-08-13 17:12:17 -0700

    trying EJS again

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ var express = require('express');
 
 var app = express();
 
+var ejs = require('ejs');
+
 var port = process.env.PORT || 5000;
 
 app.listen(port, function() {
-           console.log("Listening on " + port);
-           });
+    console.log("Listening on " + port);
+});
 
 app.get('/', function(request, response) {
-        response.render('index.ejs', {});
-        });
+    response.render('index.ejs', {});
+});
 
 /*var pg = require('pg');
 
